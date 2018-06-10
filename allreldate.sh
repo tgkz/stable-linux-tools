a2f9a3d0df03da3a7d07f174981b277b075a327f
Tsugikazu Shibata <ts@tsden.org>
2018-06-10 21:44:14 +0900

    add 2.6.12 to 2.6.??

--- allreldate.sh
+++ allreldate.sh
@@ -7,11 +7,10 @@
 # set stable-linux git repo as below
 DIR=$HOME/Projects/linux/linuxCI/src/gitrepo/linux-stable
 cd $DIR
-ver=`git tag -l v[0-9].? v[0-9].?? |sort --version-sort `
-for i in  $ver
+ver=`git tag -l v2.6.1[2-9] v2.6.[2-9]? v[0-9].? v[0-9].??|sort --version-sort`
+for i in $ver
 do 
-  echo  -n $i " "
-  #git log -1 --pretty=format:%ai $i
+  echo -n $i " "
   set `git log -1 --pretty=format:%ai $i`; echo $1
 done
 
