a5512dbace1bb24c44d6bd14ab720a504a6927f3
Tsugikazu Shibata <ts@tsden.org>
2018-06-15 18:14:00 +0900

    add -l option to count # of lines and files

--- rdatecore.sh
+++ rdatecore.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-# release date core 
-# usage rdatecre versions
+# print release date (this is core portion)
+# usage [-l] rdatecre versions
 #    ex. rdatecre 4.10 4.11.2 
 #     or rdatecore v4.10 v4.11.2
+#     -l : print # of lines and files by using gitlinecount
 #  this will show the release date for specified release tag
 #  Note: Currend directory must be a git repo
+
+line=""
+if [ $1 = '-l' ]; then
+  line='yes'
+  shift
+fi
 
 # set stable-linux git repo into $DIR as below
 DIR=$HOME/Projects/linux/linuxCI/src/gitrepo/linux-stable
@@ -16,6 +23,11 @@ do
    else v=v$i   # adding "v" for tag name in the repo
    fi
    echo -n $v " "
-   set `git log -1 --pretty=format:%ai $v`; echo $1
+   set `git log -1 --pretty=format:%ai $v`; echo -n $1
+   if [ $line = 'yes' ]; then 
+       echo -ne '\t'
+       gitlinecount.sh . $v 
+   else echo "else"  # print /n 
+   fi
 done
 
